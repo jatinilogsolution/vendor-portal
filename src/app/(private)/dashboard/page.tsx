257eb682714b5c98feaca7b5f4c815e08ff69363
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2025-10-30 18:33:07 +0530

    Done for new build

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,19 +1,30 @@
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 export default function Page() {
   return (
 
-       <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6  ">
-          {/* <SectionCards /> */}
-          {/* <div className="px-4 lg:px-6"> */}
-            {/* <ChartAreaInteractive /> */}
-          {/* </div> */}
-          {/* <DataTable data={data} /> */}
+    <div className="@container/main flex flex-1 flex-col gap-2">
+      <div className="flex flex-col gap-4 py-4 md:gap-6  ">
+        {/* <SectionCards /> */}
+        {/* <div className="px-4 lg:px-6"> */}
+        {/* <ChartAreaInteractive /> */}
+        {/* </div> */}
+        {/* <DataTable data={data} /> */}
 
-          <p>Dashboard</p>
+        <h2 className="scroll-m-20 text-center border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Your dashboard will be ready shortly!
+        </h2>
+        <p className="  text-center leading-7 [&:not(:first-child)]:mt-6">
+          Preparing your dashboard and loading the latest data…
+        </p>
+
+        <div className=" flex items-center justify-center">
+          <Spinner variant="bars" />
+
         </div>
       </div>
- 
+    </div>
+
 
   )
 }
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -14,12 +14,42 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { SideBarData } from "@/utils/constant"
+import { useRouter } from "next/navigation"
+import { signOut } from "@/lib/auth-client"
+import React from "react"
+import { toast } from "sonner"
+import { Loader2 } from "lucide-react"
+import { IconLogout } from "@tabler/icons-react"
 export function SiteHeader(porps: any) {
 
   const path = usePathname()
+  const router = useRouter();
 
   const currentNav = SideBarData.navMain.find(item => item.url === path);
 
+  // const pathname = usePathname()
+
+
+  const [isPending, setIsPending] = React.useState(false);
+  async function handleClick() {
+    await signOut({
+      fetchOptions: {
+        onRequest: () => {
+          setIsPending(true);
+        },
+        onResponse: () => {
+          setIsPending(false);
+        },
+        onError: (ctx) => {
+          toast.error(ctx.error.message);
+        },
+        onSuccess: () => {
+          toast.success("You’ve logged out. See you soon!");
+          router.push("/auth/login");
+        },
+      },
+    });
+  }
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -43,7 +73,10 @@ export function SiteHeader(porps: any) {
               <DropdownMenuLabel className=" bg-foreground/10">My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem asChild ><Link href={"/profile"}>Profile</Link></DropdownMenuItem>
-              <DropdownMenuItem className=" text-red-500">Logout</DropdownMenuItem>
+
+             
+              <DropdownMenuItem onClick={handleClick} className=" text-red-500">  {isPending && <Loader2 className="animate-spin text-red-500" /> }
+                            Log out</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
